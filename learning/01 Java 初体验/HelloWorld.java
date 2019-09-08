7dabcb2a1153b2deddaa1ec8f2d6c3b7d74420d2
evan.git <46261911@qq.com>
2019-09-08 16:05:49 +0800

    Java 初体验

--- learning/01 Java 初体验/HelloWorld.java
+++ learning/01 Java 初体验/HelloWorld.java
@@ -1,3 +1,13 @@
+/*
+ * HelloWorld.java
+ *
+ * 编译：
+ * javac -encoding utf-8 HelloWorld.java
+ *
+ * 运行：
+ * java HelloWorld
+ *
+ */
 public class HelloWorld {
     public static void main(String []args) {
         System.out.println("Welecome to JAVA world!");
